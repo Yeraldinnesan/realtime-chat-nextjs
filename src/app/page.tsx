5a3a941fc9d03b7bad971a6fe050a7c4e2813aac
Yeraldinnesan <ysanabria92@hotmail.com>
2023-04-17 16:52:39 -0500

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+import Button from "@/components/ui/Button";
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-
-  return <main className="text-red-500">hello world</main>;
+  return <Button>Hello</Button>;
 }
