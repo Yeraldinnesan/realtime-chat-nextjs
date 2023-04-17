a36863ec1319dff57d644b9c0e881680c30e4422
Yeraldinnesan <ysanabria92@hotmail.com>
2023-04-17 13:02:42 -0500

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import React, { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button isLoading />;
+};
+
+export default page;
