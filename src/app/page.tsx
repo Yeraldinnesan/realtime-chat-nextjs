4538f7e8f80b8aeb8d8d9961904f0f9348902365
Yeraldinnesan <ysanabria92@hotmail.com>
2023-04-17 14:04:24 -0500

    db integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { db } from "@/lib/db";
 
-const inter = Inter({ subsets: ["latin"] });
+export default async function Home() {
+  await db.set("hello", "hello");
 
-export default function Home() {
   return <main className="text-red-500">hello world</main>;
 }
